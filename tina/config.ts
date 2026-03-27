8ebe161589ebd9093f16da4e0dc25b53dd41938a
guineztom <guinez.tom@gmail.com>
2026-03-27 11:22:29 +0100

    Set 'id' field as required in config
    Made the 'id' field required in the configuration.

--- tina/config.ts
+++ tina/config.ts
@@ -297,7 +297,7 @@ export default defineConfig({
         path: "content/secteurs",
         format: "json",
         fields: [
-          { type: "string", name: "id", label: "ID", isTitle: true },
+          { type: "string", name: "id", label: "ID", isTitle: true, required: true},
           { type: "string", name: "label", label: "Label menu" },
           { type: "string", name: "headline", label: "Titre de la page" },
           { type: "string", name: "sub", label: "Description", ui: { component: "textarea" } },
